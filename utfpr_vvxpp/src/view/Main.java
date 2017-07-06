08be6ff40e3703e4dbe8e2c4ddbee0e01a5afb70
reinaldof <reinaldofs95@gmail.com>
2017-07-06 17:24:05 -0300

    Incrementado exemplo

--- utfpr_vvxpp/src/view/Main.java
+++ utfpr_vvxpp/src/view/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Cobertura X++");
         primaryStage.setScene(new Scene(root, 1100, 575));
         primaryStage.show();
     }
