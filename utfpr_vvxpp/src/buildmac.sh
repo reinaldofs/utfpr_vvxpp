71f7db86239070a7e33582ea640772dbb6c7b856
reinaldof <reinaldofs95@gmail.com>
2017-07-06 21:59:19 -0300

    Restruturado

--- utfpr_vvxpp/src/buildmac.sh
+++ utfpr_vvxpp/src/buildmac.sh
@@ -1,7 +1,7 @@
 fname=`echo $2 | cut -d\. -f1`
 fname=${fname}
 
-mkdir output
+mkdir ../output
 javac parser/langX.java
 javac langXrt/Runtime.java
 java parser.langX "$1"
@@ -9,12 +9,12 @@ java -jar ~/jasmin-2.4/jasmin.jar "$1.$fname.jas"
 clear
 java -classpath .:. $fname
 
-mkdir "output/$3"
+mkdir "../output/$3"
 
-cp "$1.arvore.txt" "output/$3"
-cp "$fname.class" "output/$3"
-cp "saida.txt" "output/$3"
-cp "$1" "output/$3"
+cp "$1.arvore.txt" "../output/$3"
+cp "$fname.class" "../output/$3"
+cp "saida.txt" "../output/$3"
+cp "$1" "../output/$3"
 
 java -jar cobertura.jar
 
